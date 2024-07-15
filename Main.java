6506ab46b9fcf4f1fc4c6ec6e93a273a47b3035e
jnchiarot <natalina.chiarot@keyin.com>
2024-07-15 16:42:12 -0230

    Palindrome method completed.

--- Main.java
+++ Main.java
@@ -2,19 +2,38 @@ package Stacks;
 
 public class Main {
     public static void main(String[] args) {
-        StackArrays stackArrays = new StackArrays(5);
-        stackArrays.push(1);
-        stackArrays.push(2);
-        stackArrays.push(3);
-        System.out.println(stackArrays);
-        stackArrays.pop();
-        System.out.println(stackArrays);
-        stackArrays.peek();
-        stackArrays.isEmpty();
-        stackArrays.isFull();
-        stackArrays.deleteStack();
-        System.out.println(stackArrays);
-        stackArrays.isEmpty();
+//        StackArrays stackArrays = new StackArrays(5);
+//        stackArrays.push(1);
+//        stackArrays.push(2);
+//        stackArrays.push(30);
+//        stackArrays.push(10);
+//        System.out.println(stackArrays);
+//        stackArrays.pop();
+//        System.out.println(stackArrays);
+//        stackArrays.peek();
+//        stackArrays.isEmpty();
+//        stackArrays.isFull();
+//        stackArrays.deleteStack();
+//        System.out.println(stackArrays);
+//        stackArrays.isEmpty();
+
+        StringStackArrays stringStackArrays = new StringStackArrays(5);
+        stringStackArrays.push("racecar");
+        stringStackArrays.push("apple");
+        stringStackArrays.palindrome(0);
+        stringStackArrays.palindrome(1);
+//        stringStackArrays.push("orange");
+//        stringStackArrays.push("banana");
+//        stringStackArrays.push("kelp");
+//        System.out.println(stringStackArrays);
+//        stringStackArrays.pop();
+//        System.out.println(stringStackArrays);
+//        stringStackArrays.peek();
+//        stringStackArrays.isEmpty();
+//        stringStackArrays.isFull();
+//        stringStackArrays.deleteStack();
+//        System.out.println(stringStackArrays);
+//        stringStackArrays.isEmpty();
 
     }
 }
